11ef26b14d9b8ed738852ff6358fefa15a969122
Denchik-koss <kustikov18@gmail.com>
2025-04-08 08:29:05 +0300

    add pole

--- rspoday18/Hotel.cs
+++ rspoday18/Hotel.cs
@@ -10,13 +10,14 @@ namespace rspoday18
     {
 
 
-
+        
         public int id { get; set; }
         public string Name { get; set; }
         public int CountOfStars { get; set; }
         public string CountryName { get; set; }
         public string HotelImage { get; set; }
 
+        public string Den { get ; set; }
 
     }
 
